6902b1c16fc0aaaa7b5d4b8dc0ccbdf9ecca9480
mimi <hongmikyeong11@gmail.com>
2022-10-08 20:16:04 +0900

    testing ci/cd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App">"hello world"</div>;
+  return (
+    <div className="App">
+      "hello world"
+      <div>첫 빌드 성공</div>
+    </div>
+  );
 }
 
 export default App;
